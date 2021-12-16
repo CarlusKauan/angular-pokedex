08d4660e0d701f462e3a0f65c5ffaa719464ff44
carloskauan <carloskauan190@gmail.com>
2021-12-16 15:39:29 -0300

    Criando as rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './pages/routing.module';
 import { AppComponent } from './app.component';
 
+//Modules
+import { PagesModule } from './pages/pages.module';
+
+//Components
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
