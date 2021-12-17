be1ebaf0a11248d8931a82866029383dc5aed3e0
carloskauan <carloskauan190@gmail.com>
2021-12-17 13:36:59 -0300

    Finalizando - Consumindo API

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -13,7 +13,9 @@ export class PokeListComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.pokeApiService.apiListAllPokemons.subscribe(res => res);
+    this.pokeApiService.apiListAllPokemons.subscribe(
+      res => console.log(res)
+    );
   }
 
 }
